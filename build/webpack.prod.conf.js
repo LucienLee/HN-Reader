afec8d3c2f486dfab836d727588fb7f9d5d7cda5
Lucien Lee <lkiral7903@gmail.com>
2017-09-14 19:01:57 +0800

    setup sw precache web font

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -106,7 +106,17 @@ var webpackConfig = merge(baseWebpackConfig, {
       filename: 'service-worker.js',
       staticFileGlobs: ['dist/**/*.{js,html,css}'],
       minify: true,
-      stripPrefix: 'dist/'
+      stripPrefix: 'dist/',
+      runtimeCaching: [
+        {
+          urlPattern: /^https:\/\/fonts\.googleapis\.com\//,
+          handler: 'cacheFirst'
+        },
+        {
+          urlPattern: /^https:\/\/fonts\.gstatic\.com\//,
+          handler: 'cacheFirst'
+        }
+      ]
     })
   ]
 })
